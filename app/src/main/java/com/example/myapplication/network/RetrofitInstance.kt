a792b5939ad0c5e36d7ae4bcf8f9e638f62dc852
antonio.agasantos05 <aagomes0505@gmail.com>
2024-11-21 23:39:59 -0300

    Finalizando APP

--- app/src/main/java/com/example/myapplication/network/RetrofitInstance.kt
+++ app/src/main/java/com/example/myapplication/network/RetrofitInstance.kt
@@ -7,7 +7,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    private const val BASE_URL = "https://api-java-devops.azurewebsites.net"
+    private const val BASE_URL = "https://api-java-devops.azurewebsites.net/"
 
     // Client HTTP configurado com autenticação JWT
     private fun createClient(context: Context): OkHttpClient {
